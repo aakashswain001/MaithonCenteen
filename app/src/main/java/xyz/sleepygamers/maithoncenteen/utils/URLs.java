acca6bd49347e0cd59da73534e6591a0a4f45e44
Sanghamitra1234 <sanghota4567@gmail.com>
2018-06-09 02:45:04 +0530

    menu layout made.

--- app/src/main/java/xyz/sleepygamers/maithoncenteen/utils/URLs.java
+++ app/src/main/java/xyz/sleepygamers/maithoncenteen/utils/URLs.java
@@ -2,9 +2,9 @@ package xyz.sleepygamers.maithoncenteen.utils;
 
 public class URLs {
 
-   // private static final String ROOT_URL = "http://sleepygamers.xyz/tatapower/";
+   private static final String ROOT_URL = "http://sleepygamers.xyz/tatapower/";
 
-    private static final String ROOT_URL = "http://192.168.1.101/tatapower/";
+    //private static final String ROOT_URL = "http://192.168.1.101/tatapower/";
 
     public static final String URL_REGISTER = ROOT_URL + "signup.php";
     public static final String URL_LOGIN = ROOT_URL + "login.php";
