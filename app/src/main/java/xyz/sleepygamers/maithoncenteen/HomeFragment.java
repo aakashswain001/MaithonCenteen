980c54dc4d88ba24c0ec00c5427c789cc34c9a58
Sanghamitra1234 <sanghota4567@gmail.com>
2018-06-10 01:35:17 +0530

    breakfast_view added.

--- app/src/main/java/xyz/sleepygamers/maithoncenteen/HomeFragment.java
+++ app/src/main/java/xyz/sleepygamers/maithoncenteen/HomeFragment.java
@@ -8,6 +8,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import xyz.sleepygamers.maithoncenteen.utils.SharedPrefManager;
 
@@ -15,15 +16,18 @@ import xyz.sleepygamers.maithoncenteen.utils.SharedPrefManager;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener {
 
     View mView;
+    LinearLayout breakfast;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         mView = inflater.inflate(R.layout.fragment_home, container, false);
+        breakfast=mView.findViewById(R.id.breakfast);
+        breakfast.setOnClickListener(this);
         return mView;
     }
 
@@ -32,5 +36,15 @@ public class HomeFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         //you can set the title for your toolbar here for different fragments different titles
         getActivity().setTitle("Home");
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i;
+        switch (v.getId()){
+            case R.id.breakfast:i=new Intent(getContext(), BreakfastActivity.class);
+            startActivity(i);
+            break;
+        }
     }
 }
