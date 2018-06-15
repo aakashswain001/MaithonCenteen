5ac774c14c52f67b608b9a427f7daad4d5b13ab2
Akash Ranjan Swain <aakashswain001@gmail.com>
2018-06-15 23:26:40 +0530

    Fixed recyclerview in breakfast view activity

--- app/src/main/java/xyz/sleepygamers/maithoncenteen/utils/URLs.java
+++ app/src/main/java/xyz/sleepygamers/maithoncenteen/utils/URLs.java
@@ -2,10 +2,12 @@ package xyz.sleepygamers.maithoncenteen.utils;
 
 public class URLs {
 
-   private static final String ROOT_URL = "http://sleepygamers.xyz/tatapower/";
+    private static final String ROOT_URL = "http://sleepygamers.xyz/tatapower/";
 
     //private static final String ROOT_URL = "http://192.168.1.101/tatapower/";
 
     public static final String URL_REGISTER = ROOT_URL + "signup.php";
     public static final String URL_LOGIN = ROOT_URL + "login.php";
+    public static final String URL_BREAKFAST = ROOT_URL + "foodmenu.php?apicall=getpics&category=";
+
 }
