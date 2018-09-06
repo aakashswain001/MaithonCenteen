0ba5c778586564ad7e70d0f3ed6d6a521d73bdef
Akash Ranjan Swain <aakashswain001@gmail.com>
2018-09-06 08:42:26 +0530

    Order fixed

--- app/src/main/java/xyz/sleepygamers/maithoncenteen/CheckoutActivity.java
+++ app/src/main/java/xyz/sleepygamers/maithoncenteen/CheckoutActivity.java
@@ -1,6 +1,5 @@
 package xyz.sleepygamers.maithoncenteen;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
@@ -22,25 +21,22 @@ import com.android.volley.toolbox.StringRequest;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 import xyz.sleepygamers.maithoncenteen.models.Order;
-import xyz.sleepygamers.maithoncenteen.models.User;
 import xyz.sleepygamers.maithoncenteen.models.foodmenu;
 import xyz.sleepygamers.maithoncenteen.utils.MySingleton;
-import xyz.sleepygamers.maithoncenteen.utils.SharedPrefManager;
 import xyz.sleepygamers.maithoncenteen.utils.URLs;
 
 public class CheckoutActivity extends AppCompatActivity {
     String type;
     RecyclerView recyclerView;
     CheckoutAdapter checkoutAdapter;
-    String date,orderlist;
-    int tot_price =0;
+    String date, orderString="",deliveryType;
+    int tot_price = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,7 +44,6 @@ public class CheckoutActivity extends AppCompatActivity {
         ArrayList<foodmenu> menuList;
         menuList = (ArrayList<foodmenu>) getIntent().getSerializableExtra("menuList");
         type = getIntent().getStringExtra("type");
-
 
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -65,61 +60,51 @@ public class CheckoutActivity extends AppCompatActivity {
         checkoutAdapter = new CheckoutAdapter(CheckoutActivity.this, menuList);
         recyclerView.setAdapter(checkoutAdapter);
 
-        for (foodmenu m:menuList) {
+        for (foodmenu m : menuList) {
             tot_price += m.getCount() * Integer.parseInt(m.getPrice());
         }
 
 
         //getting the menu list as comma separated string
-        for(int i=0;i<menuList.size();i++){
-            orderlist=orderlist+menuList.get(i)+",";
+        for (int i = 0; i < menuList.size(); i++) {
+            String singleOrderString = menuList.get(i).getName() + " " + menuList.get(i).getPrice() + " " + Integer.toString(menuList.get(i).getCount());
+            if (i != menuList.size() - 1)
+                orderString += singleOrderString + ",";
+            else
+                orderString += singleOrderString;
         }
 
-        TextView totPrice  = findViewById(R.id.tv_totprice);
+        TextView totPrice = findViewById(R.id.tv_totprice);
         totPrice.setText(Integer.toString(tot_price));
         Button checkout = findViewById(R.id.checkout);
         final RadioGroup rg = findViewById(R.id.rg_order);
         checkout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(rg.getCheckedRadioButtonId() == R.id.choice1){
-
-                }else{
-
+                if (rg.getCheckedRadioButtonId() == R.id.choice1) {
+                    deliveryType = "home";
+                } else {
+                    deliveryType = "normal";
                 }
                 placeOrder();
             }
         });
     }
+
     //for putting into order database
     private void placeOrder() {
-
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, URLs.URL_ORDER,
+        String url = URLs.URL_ORDER.concat("?apicall=add");
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         try {
                             //converting response to json object
                             JSONObject obj = new JSONObject(response);
-
                             Toast.makeText(getApplicationContext(), obj.getString("message"), Toast.LENGTH_SHORT).show();
                             //if no error in response
                             if (!obj.getBoolean("error")) {
-
-                                //getting the user from the response
-                                JSONObject userJson = obj.getJSONObject("order");
-
-                                //creating a new user object
-                                Order order = new Order(
-                                        userJson.getInt("id"),
-                                        userJson.getInt("user_id"),
-                                        userJson.getString("order_details"),
-                                        userJson.getString("order_date"),
-                                        userJson.getInt("price"),
-                                        userJson.getString("order_type"),
-                                        userJson.getString("status")
-
-                                );
+                                finish();
                             }
                         } catch (JSONException e) {
                             e.printStackTrace();
@@ -141,15 +126,12 @@ public class CheckoutActivity extends AppCompatActivity {
             @Override
             protected Map<String, String> getParams() {
                 Map<String, String> params = new HashMap<String, String>();
-                String user_id=getIntent().getStringExtra("user_id");
-                date = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
-                params.put("user_id",Integer.toString(1235));
-                params.put("order_details",orderlist);
-                params.put("order_date",date);
-                params.put("price",Integer.toString(tot_price));
-                params.put("order_type",type);
-              //  params.put("status",###);
-               return params;
+                params.put("user_id", Integer.toString(1235));
+                params.put("order_details", orderString);
+                params.put("price", Integer.toString(tot_price));
+                params.put("order_type", type);
+                params.put("delivery_type",deliveryType);
+                return params;
             }
         };
 
